f3167fadaf871110b2c7f7c367627dd274544784
chao.liu <chao.liu@baidao.com>
2018-03-22 10:04:07 +0800

    使用google architecture 作为项目架构

---
+++ app/build/generated/source/dataBinding/debug/android/databinding/layouts/DataBindingInfo.java
@@ -0,0 +1,6 @@
+package android.databinding.layouts;
+
+import android.databinding.BindingBuildInfo;
+
+@BindingBuildInfo(buildId="ec4f1d25-6f13-4798-8a1e-64ff11dac0d1")
+public class DataBindingInfo {}
